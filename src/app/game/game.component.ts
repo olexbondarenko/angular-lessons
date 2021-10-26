49a112b351675a9a51124fc6d9391c364121b320
Oleksandr Bondarenko <olexbondarenko@gmail.com>
2021-10-27 01:20:09 +0300

    Update lesson 2

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,126 +1,114 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Player {
+  name: string;
+  score: number;
+  value: number;
+  roundResult: string;
+}
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
 })
-export class GameComponent implements OnInit {
-  playerWin = "";
-  currentRaund = 0;
-  isGameStarted: Boolean = false;
-  isGameOver: Boolean = false;
-  isFightStarted: Boolean = false;
 
-  player1 = {
-    id: 1,
+export class GameComponent {
+  playerWin = "";
+  currentRound = 0;
+  isGameStarted = false;
+  isGameOver = false;
+  isFightStarted = false;
+
+  matrixAssociations: any = {
+    1: [3], // rock
+    2: [1], // scissors
+    3: [2], // paper
+  }
+
+  computer: Player = {
     name: "Computer",
     score: 0,
     value: 0,
-    raundResult: 0
+    roundResult: ""
   }
 
-  player2 = {
-    id: 2,
-    name: {
-      value: "",
-      hasError: false
-    },
+  user: Player = {
+    name: "",
     score: 0,
     value: 0,
-    raundResult: 0
-  };
+    roundResult: ""
+  }
 
+  // Generates random value from min to max
   getRandom(min: number, max: number) {
     let random = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(random);
   }
 
+  // Checks if user name value not empty and start the game
   startGame(): void {
-    if (this.player2.name.value.length < 3) {
-      this.player2.name.hasError = true;
-    }
-    else {
-      this.player2.name.value.trim();
+    if (this.user.name.length !== 0) {
+      this.user.name.trim();
       this.isGameStarted = true;
     }
   }
 
-  startFight(selectedValue: number): void {
+  // Starts round fight, where player.value: 
+  startFight(userValue: number): void {
     this.isFightStarted = true;
-    this.currentRaund += 1;
+    this.currentRound += 1;
 
     setTimeout(() => {
-      this.player1.value = this.getRandom(1, 3);
-      this.player2.value = selectedValue;
+      this.computer.value = this.getRandom(1, 3);
+      this.user.value = userValue;
 
-      if ((this.player1.value === 1 && this.player2.value === 2) ||
-        (this.player1.value === 2 && this.player2.value === 3) ||
-        (this.player1.value === 3 && this.player2.value === 1)) {
-        this.player1.score += 1;
-        this.player1.raundResult = 1;
-        this.player2.raundResult = 2;
+      if (this.computer.value == userValue) {
+        this.computer.roundResult = "draw";
+        this.user.roundResult = "draw";
       }
-      else if ((this.player1.value === 2 && this.player2.value === 1) ||
-        (this.player1.value === 3 && this.player2.value === 2) ||
-        (this.player1.value === 1 && this.player2.value === 3)) {
-        this.player2.score += 1;
-        this.player2.raundResult = 1;
-        this.player1.raundResult = 2;
+      else if (!this.matrixAssociations[this.computer.value].includes(userValue)) {
+        this.computer.score += 1;
+        this.computer.roundResult = "won";
+        this.user.roundResult = "lost";
       }
       else {
-        this.player1.raundResult = 3;
-        this.player2.raundResult = 3;
+        this.user.score += 1;
+        this.user.roundResult = "won";
+        this.computer.roundResult = "lost";
       }
+
       setTimeout(() => {
-        this.player1.value = 0;
-        this.player1.raundResult = 0;
-        this.player2.value = 0;
-        this.player2.raundResult = 0;
+        this.computer.value = 0;
+        this.computer.roundResult = "";
+        this.user.value = 0;
+        this.user.roundResult = "";
         this.isFightStarted = false;
 
-        if (this.currentRaund === 3) {
+        if (this.currentRound === 3) {
           this.isGameOver = true;
-          if (this.player1.score > this.player2.score) {
-            this.playerWin = this.player1.name
+          if (this.computer.score > this.user.score) {
+            this.playerWin = this.computer.name + " wins!"
           }
-          else if (this.player1.score < this.player2.score) {
-            this.playerWin = this.player2.name.value
+          else if (this.computer.score < this.user.score) {
+            this.playerWin = this.user.name + " wins!"
           }
           else {
-            this.playerWin = "Draw"
+            this.playerWin = "Draw!"
           }
         }
       }, 1500);
-    }, 2500);
+    }, 1500);
   }
 
-  raundResult(player: any) {
-    if (player.roundResult === 1) {
-      return "won"
-    }
-    else if (player.roundResult === 2) {
-      return "lost"
-    }
-    else if (player.roundResult === 3) {
-      return "draw"
-    }
-    else {
-      return ""
-    }
-  }
-
+  // Returns the game to its starting positions
   retryGame(): void {
     this.isGameOver = false;
     this.isFightStarted = false;
-    this.currentRaund = 0;
-    this.player1.value = 0;
-    this.player1.score = 0;
-    this.player2.value = 0;
-    this.player2.score = 0;
-  }
-
-  ngOnInit(): void {
-
+    this.currentRound = 0;
+    this.computer.value = 0;
+    this.computer.score = 0;
+    this.user.value = 0;
+    this.user.score = 0;
   }
 }
