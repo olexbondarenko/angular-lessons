545cc29143fdd7b40bb1aeedbc8d674b9665ed0b
Oleksandr Bondarenko <olexbondarenko@gmail.com>
2021-10-28 21:30:11 +0300

    Updated lesson3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { PositiveNumbersDirective } from './positive-numbers.directive';
+import { ReverseStringPipe } from './reverse-string.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PositiveNumbersDirective,
+    ReverseStringPipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reverse-string.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reverseString'
+})
+export class ReverseStringPipe implements PipeTransform {
+  transform(string: string, reverseCount: number): string {
+    var reversedString = "";
+    var arrSymbols = string.split("");
+    if (+reverseCount === 0 || +reverseCount >= string.length) {
+      reversedString = arrSymbols.reverse().join("");
+    }
+    else {
+      let arrStart = arrSymbols.slice(0, arrSymbols.length - reverseCount);
+      let arrEnd = arrSymbols.slice(-reverseCount).reverse();
+      reversedString = arrEnd.concat(arrStart).join("");
+    }
+    return reversedString;
+  }
+}
