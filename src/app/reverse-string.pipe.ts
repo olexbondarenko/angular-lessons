25835af19bc66ef3c61fbe3b64a1a06bff43b430
Oleksandr Bondarenko <olexbondarenko@gmail.com>
2021-10-31 22:54:27 +0200

    Updated lesson3

--- src/app/reverse-string.pipe.ts
+++ src/app/reverse-string.pipe.ts
@@ -6,15 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class ReverseStringPipe implements PipeTransform {
   transform(string: string, reverseCount: number): string {
-    var reversedString = "";
-    var arrSymbols = string.split("");
+    let reversedString = "";
+    let arrSymbols = string.split("");
     if (+reverseCount === 0 || +reverseCount >= string.length) {
       reversedString = arrSymbols.reverse().join("");
     }
     else {
-      let arrStart = arrSymbols.slice(0, arrSymbols.length - reverseCount);
-      let arrEnd = arrSymbols.slice(-reverseCount).reverse();
-      reversedString = arrEnd.concat(arrStart).join("");
+      let arrStart = arrSymbols.slice(0, reverseCount).reverse();
+      let arrEnd = arrSymbols.slice(reverseCount, arrSymbols.length);
+      reversedString = arrStart.concat(arrEnd).join("");
     }
     return reversedString;
   }
